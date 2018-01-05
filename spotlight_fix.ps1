4e4add199fc5067ee574b28827a9438970fffac1
ArtmannMX <artmx@protonmail.ch>
2018-01-05 17:56:25 -0600

    Update spotlight_fix.ps1

--- spotlight_fix.ps1
+++ spotlight_fix.ps1
@@ -1,2 +1,5 @@
+<# Script para resolver el problema del contenido destacado de windows 
+(spotlight) en la pantalla de bloqueo atascado en una sola imagen #>
+#
 Get-AppxPackage -allusers *ContentDeliveryManager* |
 foreach {Add-AppxPackage "$($_.InstallLocation)\appxmanifest.xml" -DisableDevelopmentMode -register }
